78beaa4da2de1acc1fd64e6e86ce60bfd67e6cba
xiaojia_fish <xiaojia_fish@126.com>
2013-05-29 14:59:15 +0800

    up kf slide

--- static/kf/tab1.0.js
+++ static/kf/tab1.0.js
@@ -1,44 +1,42 @@
 kf['tab'] = function(obj, config){
-	var tabConfig = {
-		'eventType': 'mouseover'
+	this.config = {
+		eventType: 'mouseover',
+		index: 0
 	};
 	if (config) {
-		$.extend(tabConfig, config);
-	}	
-	var tabObj;
+		$.extend(this.config, config);
+	}
 	if (typeof(obj) == 'object') {
-		tabObj = obj;
+		this.obj = obj;
 	} else {
-		tabObj = $('#' + obj);
+		this.obj = $('#' + obj);
 	}
-	var tabNav = tabObj.find('.kf-tab-nav');
-	var tabOption = tabNav.find('.kf-tab-option');
-	if (tabOption.length == 0) {
-		tabOption = tabNav.find('li');
+	this.nav = this.obj.find('.kf-tab-nav');
+	this.option = this.nav.find('.kf-tab-option');
+	if (this.option.length == 0) {
+		this.option = this.nav.find('li');
 	}
-	var tabContent = tabObj.find('.kf-tab-content');
-	var tabPannel = tabContent.find('.kf-tab-pannel');
-	if (tabPannel.length == 0) {
-		tabPannel = tabContent.find('li');
+	this.content = this.obj.find('.kf-tab-content');
+	this.pannel = this.content.find('.kf-tab-pannel');
+	if (this.pannel.length == 0) {
+		this.pannel = this.content.find('li');
 	}
-	tabOption.each(function(index, item){
-		$(item).on(tabConfig['eventType'], function(){
-			tabOption.removeClass('current');
-			$(this).addClass('current');
-			tabPannel.removeClass('current');
-			tabPannel.eq(index).addClass('current');
-		});
-	});
-	this.obj = tabObj;
-	this.option = tabOption;
-	this.pannel = tabPannel;
 	
-	this.option.removeClass('current');
-	this.option.eq(0).addClass('current');
-	this.pannel.removeClass('current');
-	this.pannel.eq(0).addClass('current');
+	this.show(this.config['index']);
+	this.init();
 };
 kf.augment(kf['tab'], {
+	init: function(){
+		var _this = this;
+		this.option.each(function(index, item){
+			$(item).on(_this.config['eventType'], function(){
+				_this.option.removeClass('current');
+				$(this).addClass('current');
+				_this.pannel.removeClass('current');
+				_this.pannel.eq(index).addClass('current');
+			});
+		});
+	},
 	show: function(index){
 		if (!index) {
 			index = 0;
