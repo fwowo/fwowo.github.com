c6421c17f7aa6434cc1876b5d8f3b0e7eafccda0
xiaojia_fish <xiaojia_fish@126.com>
2013-06-03 17:23:30 +0800

    up waterfall

--- static/js/index.js
+++ static/js/index.js
@@ -1,48 +1,17 @@
-$(function(){
-	var wrapWidth = $('.u-wrap').width() - 20;
-	var tdWidth = $('.u-td').width();
-	var tdLen = Math.floor(wrapWidth / tdWidth);
-	$('.u-wrap').width(tdWidth * tdLen);
-	var tdObj = [];
-	$('.u-wrap').html('');
-	for (i=0; i<tdLen; i++) {
-		tdObj[i] = {
-			"obj": $('<div class="u-td"></div>'),
-			"height": 0
-		};
-		$('.u-wrap').append(tdObj[i].obj);
-	}
-	var minIndex = function(){
-		var index = 0;
-		for (i=0; i<tdLen; i++) {
-			if (tdObj[i].height < tdObj[index].height) {
-				index = i;
-			}
-		}
-		return index;
-	};
-	$.getJSON("index.txt?r=" + Math.random(), function(response){
-		var data = response.data;
-		$.each(data, function(){
-			var liObj = $('<div class="wrap">\
-				<div class="contain">\
-					<div class="pic"><a target="_blank" href="' + this.url + '"><img src="' + this.pic + '"></a></div>\
-					<div class="title">' + this.title + '</div>\
-				</div>\
-			</div>');
-			var pic = new Image();
-			pic.src = this.pic;
-			if (pic.complete) {
-				var index = minIndex();
-				tdObj[index].obj.append(liObj);
-				tdObj[index].height = tdObj[index].obj.height();
-			} else {
-				pic.onload = function(){
-					var index = minIndex();
-					tdObj[index].obj.append(liObj);
-					tdObj[index].height = tdObj[index].obj.height();
-				};
-			}
-		});
+var wrapWidth = $('.u-wrap').width() - 20;
+var tdWidth = $('.u-td').width();
+var tdLen = Math.floor(wrapWidth / tdWidth);
+$('.u-wrap').width(tdWidth * tdLen);
+$('.u-wrap').html('');
+for (i=0; i<tdLen; i++) {
+	var tdObj = $('<div class="u-td kf-waterfall-column"></div>');
+	$('.u-wrap').append(tdObj);
+}
+kf['host'] = '';
+kf.use('waterfall1.0', function(){
+	var waterfall = new kf.waterfall('kj-waterfall', {
+		action: '/index.txt',
+		moreBtn: false
 	});
 });
+	
